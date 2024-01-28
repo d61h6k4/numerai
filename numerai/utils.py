ab92c5774367a652f19c02309b3d27fbe614cdd8
Danila <ddbihbka@gmail.com>
2024-01-28 12:03:05 +0100

    Optimize weight calculation

--- numerai/utils.py
+++ numerai/utils.py
@@ -5,9 +5,10 @@ from sklearn.utils.class_weight import compute_class_weight
 from numerai.dataset import get_dataset
 
 
-def compute_target_weight(
-    targets: set[str]
-) -> dict[str, np.ndarray]:
+def compute_target_weight(targets: set[str]) -> dict[str, np.ndarray]:
+    if len(targets) == 1:
+        return {"target": torch.tensor([4.1451, 0.9940, 0.4005, 0.9988, 3.9293])}
+    
     targets_values = {t: [] for t in targets}
     ds = get_dataset()
     for example in ds.shuffle().select(range(20_000)):
